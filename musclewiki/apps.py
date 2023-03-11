46c9c8baba3c630f07d089bc9d8d04ef09c14547
Mateusz <mateuszgrzegorzewski@gazeta.pl>
2023-03-11 21:40:48 +0100

    Adding automatic creating of muscle parties to Muscle model in Musclewiki module.

--- musclewiki/apps.py
+++ musclewiki/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class MusclewikiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "musclewiki"
+
+    def ready(self):
+        call_command('loaddata', 'muscles.json')
--- musclewiki/serializers.py
+++ musclewiki/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Exercise, Muscle
 
 
 class MuscleSerializer(serializers.ModelSerializer):
-    muscle_with_exercises_url = serializers.SerializerMethodField(
-        read_only=True)
+    muscle_with_exercises_url = serializers.HyperlinkedIdentityField(
+        view_name='muscles-detail')
     exercise_create_url = serializers.ReadOnlyField()
 
     class Meta:
@@ -15,13 +14,6 @@ class MuscleSerializer(serializers.ModelSerializer):
         fields = '__all__'
         validators = [UniqueTogetherValidator(
             queryset=Muscle.objects.all(), fields=['name'], message="This muscle already exist")]
-
-    def get_muscle_with_exercises_url(self, obj):
-        request = self.context.get('request')
-
-        if request is None:
-            return None
-        return reverse("muscles-detail", kwargs={"pk": obj.pk}, request=request)
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
